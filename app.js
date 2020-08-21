9ec1fe8969100ca347c58733939259a5dd1ff126
Tasha <xtasherx@gmail.com>
2020-08-20 22:44:30 -0500

    moved conditionals to test code

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
   // display the day/time at the top of page
   $("#currentDay").text(`${moment().format("dddd MMM Do YYYY ")}`);
-  const anHourAgo = moment().subtract(1, "hour").format("HHmmss");
-  const aSecAhead = moment().add(1, "second").format("HHmmss");
-  console.log(anHourAgo);
-  console.log(aSecAhead);
-  // variable to store current time in military time for conditional
-  const now = moment().format("HHmmss");
+
+  // variable to store current time in military time
+  const anHourAgo = moment().subtract(1, "hour").format("HHmmss"); // time minus 1hr
+  const aSecAhead = moment().add(1, "second").format("HHmmss"); // time plus one second
+
   // WHEN I refresh the page  --  THEN the saved events persist
 
   // retrieve event data from local storage on page load
@@ -16,27 +15,21 @@ $(document).ready(function () {
 
   // write out events on load to their timeslots
   $(".form-control").each(function (i) {
-    const eventKey = $(this).next().attr("id");
-    $(this).text(events2[eventKey]);
-
     // check to see if the current time blocks are in the past and add styling accordingly
     // find a way to check if time is in the present
-    console.log($(this).attr("data-time"));
+
     if ($(this).attr("data-time") < anHourAgo) {
       $(this).css("background-color", "slategrey");
     }
-    // time minus 1hr
+
     if ($(this).attr("data-time") > aSecAhead) {
       $(this).css("background-color", "lavenderblush");
     }
-    // time plus one second
 
-    // if ($(this).attr("data-time") < now) {
-    //   $(this).css("background-color", "lavenderblush");
-    // } else {
-    //   $(this).css("background-color", "white");
-    // }
+    const eventKey = $(this).next().attr("id");
+    $(this).text(events2[eventKey]);
   });
+
   // event listener for save button stores that timeslot variable into local storage
   $(".btn").on("click", function () {
     buttonId = $(this).attr("id");
